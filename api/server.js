3863c78807584b0036d7e2cf710fe45da79728a6
tommybrits951 <tommybrits74@gmail.com>
2023-02-26 07:38:39 -0800

    set up server

--- api/server.js
+++ api/server.js
@@ -1,10 +1,13 @@
 const express = require('express');
-
+const {logger} = require("./middleware/middleware")
+const morgan = require("morgan")
 const server = express();
+const usersRouter = require("./users/users-router")
+server.use(express.json())
 
-// remember express by default cannot parse JSON in request bodies
+server.use(morgan('dev'))
+server.use("/users", usersRouter)
 
-// global middlewares and the user's router need to be connected here
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
--- api/users/users-router.js
+++ api/users/users-router.js
@@ -1,10 +1,11 @@
+const {logger} = require("../middleware/middleware")
 const express = require('express');
-
+const Users = require("./users-model")
+const Posts = require("../posts/posts-model")
 // You will need `users-model.js` and `posts-model.js` both
 // The middleware functions also need to be required
 
 const router = express.Router();
-
 router.get('/', (req, res) => {
   // RETURN AN ARRAY WITH ALL THE USERS
 });
@@ -40,5 +41,7 @@ router.post('/:id/posts', (req, res) => {
   // this needs a middleware to verify user id
   // and another middleware to check that the request body is valid
 });
+router.use(logger)
 
 // do not forget to export the router
+module.exports = router;
