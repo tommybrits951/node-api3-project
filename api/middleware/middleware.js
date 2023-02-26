cff7481ef0cd7a98bbaaf438138b96033638b599
tommybrits951 <tommybrits74@gmail.com>
2023-02-26 08:49:31 -0800

    still working with async problems

--- api/middleware/middleware.js
+++ api/middleware/middleware.js
@@ -3,33 +3,49 @@ const Posts = require("../posts/posts-model")
 
 function logger(req, res, next) {
  const timeStamp = new Date().toLocaleString()
-  console.log(`${req.method} http://localhost:9000${req.originalUrl} ${timeStamp}`)
+  console.log(`${req.method} ${req.originalUrl} ${timeStamp}`)
  
  next()
 }
 
 async function validateUserId(req, res, next) {
-  const {id} = req.params;
- await Users.getById(id)
-  .then(user => {
-    if (user) {
-      req.user = user;
-      next()
+  try {
+    
+    const {id} = req.params;
+    const user = await Users.getById(id)
+    if (!user) {
+      res.status(404).json({message: "user not found"})
+
     } else {
-      next({status: 404, message: "user not found"})
-    }
-  })
-  .catch(err => {
-    next(err)
-  })
+      req.user = user
+   next()
+      
+  }
+  } catch (error) {
+    next({status: 500, message: error.message})
+  }
+  
 }
 
-function validateUser(req, res, next) {
-  // DO YOUR MAGIC
+async function validateUser(req, res, next) {
+  
+    const {name} = req.body;
+    if (!name) {
+      res.status(400).json({message: "missing required name field"})
+    } else {
+      req.name = name.trim()
+      next()
+    }
 }
 
 function validatePost(req, res, next) {
-  // DO YOUR MAGIC
+  const {text} = req.body;
+  if (!text) {
+    res.status(400).json({message: "missing required text field"})
+  } else {
+    req.text = text;
+    next()
+  }
 }
 
 // do not forget to expose these functions to other modules
--- api/users/users-router.js
+++ api/users/users-router.js
@@ -1,4 +1,4 @@
-const {logger, validatePost, validateUser, validateUserId} = require("../middleware/middleware")
+const {validatePost, validateUser, validateUserId} = require("../middleware/middleware")
 const express = require('express');
 const Users = require("./users-model")
 const Posts = require("../posts/posts-model")
@@ -7,54 +7,54 @@ const Posts = require("../posts/posts-model")
 
 const router = express.Router();
 
-router.get('/', async (req, res, next) => {
-  await Users.get()
+router.get('/users', (req, res, next) => {
+  Users.get()
   .then(users => {
     res.status(200).json(users)
   })
   .catch(err => next())
 });
 
-router.get('/:id', validateUserId, async (req, res, next) => {
+router.get('/users/:id', validateUserId, (req, res, next) => {
   // RETURN THE USER OBJECT
   try {
     res.status(200).json(req.user)
-  } catch {
-    next({status: 500, message: err.message})
+  } catch (error) {
+    next()
   }
   // this needs a middleware to verify user id
 });
 
-router.post('/', async (req, res) => {
+router.post('/users', validateUser, (req, res) => {
   // RETURN THE NEWLY CREATED USER OBJECT
   // this needs a middleware to check that the request body is valid
 });
 
-router.put('/:id', (req, res) => {
+router.put('/users/:id', validateUserId, validateUser, (req, res) => {
   // RETURN THE FRESHLY UPDATED USER OBJECT
   // this needs a middleware to verify user id
   // and another middleware to check that the request body is valid
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/users/:id', validateUserId, (req, res) => {
   // RETURN THE FRESHLY DELETED USER OBJECT
   // this needs a middleware to verify user id
 });
 
-router.get('/:id/posts', (req, res) => {
+router.get('/users/:id/posts', validateUserId, (req, res) => {
   // RETURN THE ARRAY OF USER POSTS
   // this needs a middleware to verify user id
 });
 
-router.post('/:id/posts', (req, res) => {
+router.post('/:id/posts', validateUserId, validatePost, (req, res) => {
   // RETURN THE NEWLY CREATED USER POST
   // this needs a middleware to verify user id
   // and another middleware to check that the request body is valid
 });
 
-router.use((err, req, res, next) => {
-  res.status(err.status || 500).json({
-    message: err.message,
+router.use((error, req, res, next) => {
+  res.status(error.status || 500).json({
+    message: error.message,
     customMessage: "Oops! Something went wrong!"
   })
 })
